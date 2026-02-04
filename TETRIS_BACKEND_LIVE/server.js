37d7c613dd5a3a7c96d4a1e860c1b2ff45045b15
VamsiBoyana <vamsiboina1800@gmail.com>
2026-02-04 12:12:42 +0530

    modified server.js

--- TETRIS_BACKEND_LIVE/server.js
+++ TETRIS_BACKEND_LIVE/server.js
@@ -1,32 +1,3 @@
-// const express = require('express')    
-// const dotenv = require('dotenv')
-// const connectToDb = require('./config/db')
-// const router = require('./routes/authRoutes')
-// const bot = require("./teleBot/bot");
-// const cors = require("cors");
-
-
-
-
-// const app = express()
-// dotenv.config()
-// app.use(cors());
-
-
-
-// connectToDb()
-
-// // Middleware
-// app.use(express.json())
-// app.use(express.urlencoded({ extended: true }));
-// app.use("/api/auth", router);
-
-
-// // Start the server
-// app.listen(process.env.PORT, () => {
-//     console.log(`Server running successfully on ${process.env.PORT}`);
-// });
-
 const express = require('express')
 const dotenv = require('dotenv')
 const connectToDb = require('./config/db')
